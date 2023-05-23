3d9e78fe3a36049cbc400894c144b2c000680a81
Alaa Barka <tmakaveli643@gmail.com>
2023-05-23 21:58:10 +0100

    creating the cat services

---
+++ src/services/category.services.ts
@@ -0,0 +1,34 @@
+import Category from "../models/category.entity"
+
+
+const saveCat = async (datas : any ) =>{
+    await Category.save(datas)
+}
+
+
+const getCat = async (id : any) =>{
+    return await Category.findOne(id)
+}
+const getCats = async (id : any) =>{
+    return await Category.find(id)
+}
+
+const updateCat = async (id : any, datas : any) =>{
+    await Category.update(id,datas)
+}
+
+
+const deleteCat = async (id : any) =>{
+    await Category.delete(id)
+}
+
+const getAll = async () => {return await Category.find()}
+
+export {
+    saveCat,
+    getCat,
+    getCats,
+    updateCat,
+    deleteCat,
+    getAll
+}
