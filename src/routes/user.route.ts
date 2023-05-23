a371b337b2d922018e5e662be0a6fe6eacdf1436
Alaa Barka <tmakaveli643@gmail.com>
2023-05-23 21:25:03 +0100

    Routing

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,11 @@
 import Router from 'express'
 import CreateUser from '../controllers/Users/create.controller'
 import Auth from '../controllers/Users/auth.controller'
+import GetMe from '../controllers/Users/getMe.controller'
 const userRoute = Router()
 
 userRoute.post("/", CreateUser)
 userRoute.post("/auth", Auth)
-
+userRoute.get("/", GetMe)
 export default userRoute
 
