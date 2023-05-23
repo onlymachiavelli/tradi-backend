16cf22a0cb0c25a28cf3e92486e0239868151800
Alaa Barka <tmakaveli643@gmail.com>
2023-05-23 20:05:29 +0100

    preparing the user services

---
+++ src/services/user.services.ts
@@ -0,0 +1,38 @@
+import Users from "../models/users.entity"
+
+
+
+const CreateUser = async (datas : any) => {
+    await Users.save(datas)
+}
+
+const getOne = async (field : any, target : any) =>{
+    return Users.findOne({
+        select:["created_at", "email", "id", "phone", "picture", "role","username","updated_at"], 
+        where : {
+            [field] : target
+        }
+    })
+    
+
+
+}
+
+
+const getPass = async (field : any, target : any) =>{
+    return await Users.findOne({
+        select:["password"],
+        where : {
+            [field] : target
+        }
+    })
+}
+
+
+
+
+export {
+    CreateUser, 
+    getOne, 
+    getPass
+}
