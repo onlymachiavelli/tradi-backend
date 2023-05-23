26817b8a4b2cd3c1b6cba176a5b641ac194e95bd
Alaa Barka <tmakaveli643@gmail.com>
2023-05-23 19:53:06 +0100

    preparing the user schema

---
+++ src/models/users.entity.ts
@@ -0,0 +1,45 @@
+import * as TypeORM from 'typeorm'
+
+
+
+//create enum 
+
+export enum UserRole {
+    Inventory = 'inv',
+    OrderManegment= "ord",
+    Admin = 'adm'
+}
+
+
+@TypeORM.Entity()
+class Users extends TypeORM.BaseEntity {
+    @TypeORM.PrimaryGeneratedColumn()
+    id : number 
+
+    @TypeORM.Column({nullable : false, unique: true }) 
+    username : string 
+
+    @TypeORM.Column({nullable : false, unique: true })
+    email : string
+
+    @TypeORM.Column({nullable : false})
+    password : string
+
+    @TypeORM.Column({nullable : false})
+    role : UserRole
+
+    @TypeORM.Column({nullable : true})
+    picture: string 
+
+    @TypeORM.Column({nullable : true})
+    phone: string
+
+    @TypeORM.Column()
+    created_at : Date
+
+    @TypeORM.Column()
+    updated_at : Date
+
+}
+
+export default Users
