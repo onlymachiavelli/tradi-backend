421d0690ab333c1cc04b0ef04cc82a08f1009a8a
Alaa Barka <tmakaveli643@gmail.com>
2023-05-26 21:56:26 +0100

    fixing an issue

--- src/controllers/Products/all.products.ts
+++ src/controllers/Products/all.products.ts
@@ -1,47 +1,48 @@
 import * as exp from 'express'
 import * as Services from './../../services/products.services'
 
-const GetAll:exp.RequestHandler = async (req,res) =>{
+const GetAll: exp.RequestHandler = async (req, res) => {
 
-    //get the datas ! 
-    const datas : any = await Services.GetAll()
-    if (req.query.filter) {
-        
-        const filter = req.query.filter as string 
-        //filter by name 
-        let response : any = []
-        datas.forEach((element : any) => {
-            if (element.title.includes(filter)) {
-                response.push(element)
-            }
-        }
-        )
+  // Get the data
+  const datas: any = await Services.GetAll()
 
-        //filter by description  
+  if (req.query.filter) {
 
-        datas.forEach((element : any) => {
-            if (element.description.includes(filter)) {
-                response.push(element)
-            }
-        }
-        )
-        res.status(200).send(response)
-        return
+    const filter = req.query.filter as string 
+
+    // Filter by name 
+    let response: any = []
+    for (const element of datas) {
+      if (element.title.toLowerCase().includes(filter.toLowerCase())) {
+        response.push(element)
+      }
     }
 
-    if (req.query.id) {
-        const id = req.query.id as string 
-        datas.forEach((element : any) => {
-            if (element.id == id) {
-                res.status(200).send(element)
-                return
-            }
-        }
-        )
-        res.status(404).send("Not Found")
-        return
+    // Filter by description  
+    for (const element of datas) {
+      if (element.description.includes(filter)) {
+        response.push(element)
+      }
     }
-    res.status(200).send(datas)
+    
+    res.status(200).send(response)
+    return
+  }
+
+  if (req.query.id) {
+    const id = req.query.id as string 
+    for (const element of datas) {
+      if (element.id == id) {
+        res.status(200).send(element)
+        return
+      }
+    }
+
+    res.status(404).send("Not Found")
+    return
+  }
+
+  res.status(200).send(datas)
 }
 
 export default GetAll
